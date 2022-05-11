23a1ce4fa5599675bbd5fdaea98099e46b3efd23
Erik Abair <erik.abair@gmail.com>
2022-05-10 19:53:45 -0700

    nv2a: Adjust NaN handling for fog coordinate to be similar to HW

--- hw/xbox/nv2a/pgraph/glsl/vsh.c
+++ hw/xbox/nv2a/pgraph/glsl/vsh.c
@@ -128,6 +128,20 @@ MString *pgraph_gen_vsh_glsl(const ShaderState *state, bool prefix_outputs)
             }
         }
     }
+
+    mstring_append(header,
+        "/* Converts the input to vec4, pads with last component */\n"
+        "vec4 _in(float v) { return vec4(v); }\n"
+        "vec4 _in(vec2 v) { return v.xyyy; }\n"
+        "vec4 _in(vec3 v) { return v.xyzz; }\n"
+        "vec4 _in(vec4 v) { return v.xyzw; }\n"
+        "\n"
+        "#define FixNaNClamped(src, mask) _FixNaNClamped(_in(src)).mask\n"
+        "vec4 _FixNaNClamped(vec4 src)\n"
+        "{\n"
+        "  return mix(src, (sign(floatBitsToInt(src)) + 1.0) * 0.5, isnan(src));\n"
+        "}\n");
+
     mstring_append(header, "\n");
 
     MString *body = mstring_from_str("void main() {\n");
@@ -232,11 +246,11 @@ MString *pgraph_gen_vsh_glsl(const ShaderState *state, bool prefix_outputs)
             break;
         }
 
-        mstring_append(body, "  oFog.xyzw = vec4(fogFactor);\n");
+        mstring_append(body, "  oFog = FixNaNClamped(fogFactor, xyzw);\n");
     } else {
         /* FIXME: Is the fog still calculated / passed somehow?!
          */
-        mstring_append(body, "  oFog.xyzw = vec4(1.0);\n");
+        mstring_append(body, "  oFog = vec4(1.0);\n");
     }
 
     /* Set outputs */
