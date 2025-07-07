0f3c70f5107d76277971377a5b34f39754a755a9
Erik Abair <erik.abair@gmail.com>
2025-07-07 16:26:10 -0700

    SQUASHME: Increases quantization.

--- hw/xbox/nv2a/pgraph/gl/vertex.c
+++ hw/xbox/nv2a/pgraph/gl/vertex.c
@@ -230,9 +230,10 @@ unsigned int pgraph_gl_bind_inline_array(NV2AState *d)
 
     nv2a_profile_inc_counter(NV2A_PROF_GEOM_BUFFER_UPDATE_2);
     glBindBuffer(GL_ARRAY_BUFFER, r->gl_inline_array_buffer);
-    glBufferData(GL_ARRAY_BUFFER,
-                 quantize_buffer_alloc(index_count * vertex_size),
-                 pg->inline_array, GL_STREAM_DRAW);
+    GLsizeiptr buffer_size = quantize_buffer_alloc(index_count * vertex_size);
+    glBufferData(GL_ARRAY_BUFFER, buffer_size, NULL, GL_STREAM_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, index_count * vertex_size,
+                    pg->inline_array);
     pgraph_gl_bind_vertex_attributes(d, 0, index_count-1, true, vertex_size,
                                   index_count-1);
 
