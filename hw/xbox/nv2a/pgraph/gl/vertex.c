748af1151ec027c7383cbd1a23532de61463a38c
Erik Abair <erik.abair@gmail.com>
2025-07-08 12:29:18 -0700

    Restore original functionality for baseline testing.

--- hw/xbox/nv2a/pgraph/gl/vertex.c
+++ hw/xbox/nv2a/pgraph/gl/vertex.c
@@ -230,8 +230,11 @@ unsigned int pgraph_gl_bind_inline_array(NV2AState *d)
 
     nv2a_profile_inc_counter(NV2A_PROF_GEOM_BUFFER_UPDATE_2);
     glBindBuffer(GL_ARRAY_BUFFER, r->gl_inline_array_buffer);
-    GLsizeiptr buffer_size = quantize_buffer_alloc(index_count * vertex_size);
-    glBufferData(GL_ARRAY_BUFFER, buffer_size, NULL, GL_STREAM_DRAW);
+//    GLsizeiptr buffer_size = quantize_buffer_alloc(index_count * vertex_size);
+//    glBufferData(GL_ARRAY_BUFFER, buffer_size, NULL, GL_STREAM_DRAW);
+
+    assert(index_count * vertex_size <= NV2A_MAX_BATCH_LENGTH_V2 * sizeof(uint32_t) && "This title uses inline arrays that are too large for this test");
+    glBufferData(GL_ARRAY_BUFFER, NV2A_MAX_BATCH_LENGTH_V2 * sizeof(uint32_t), NULL, GL_STREAM_DRAW);
     glBufferSubData(GL_ARRAY_BUFFER, 0, index_count * vertex_size,
                     pg->inline_array);
     pgraph_gl_bind_vertex_attributes(d, 0, index_count-1, true, vertex_size,
